7c2672816a591ae4d6dcccd514ecf4630cee17fa
Shaan <shaankhan101@gmail.com>
2023-04-13 23:18:12 +1000

    Added products onto routes

--- packages/backend/src/routes/products.ts
+++ packages/backend/src/routes/products.ts
@@ -1,31 +1,18 @@
 import { FastifyInstance } from "fastify";
 import * as controllers from "../controllers";
 import { isLoggedIn, isStaff } from "../helpers/auth";
-import { UserCollectionSchemaRef, UserSchemaRef } from "../schema";
 
 /*
-  CreateProduct
-  ViewAllProduct
-  ViewProduct
-  DeleteProduct
-  DeleteProducts
-  UpdateProduct
+  getProduct (GET) /:productId
+  getProducts (GET) /
+  createProduct (POST) /
+  deleteProduct (DELETE) /
+  deleteProducts (DELETE) /
+  updateProduct (PUT) /
 */
 
 export default async function productsRouter(fastify: FastifyInstance) {
-  fastify.route({
-    schema: {
-      response: {
-        200: ProductsSchemaRef,
-      },
-      operationId: "getProducts",
-      tags: ["Products"],
-    },
-    method: "GET",
-    url: "",
-    handler: controllers.products,
-  });
-
+  // getProduct (GET) /:productId
   fastify.route({
     schema: {
       response: {
@@ -51,5 +38,136 @@ export default async function productsRouter(fastify: FastifyInstance) {
     handler: controllers.products,
   });
 
+  // getProducts (GET) /
+  fastify.route({
+    schema: {
+      response: {
+        200: ProductSchemaRef,
+      },
+      params: {
+        type: "object",
+        properties: {},
+      },
+      operationId: "getProducts",
+      tags: ["Products"],
+      security: [
+        {
+          sessionid: [],
+        },
+      ],
+    },
+    method: "GET",
+    url: "",
+    preValidation: [isLoggedIn, isStaff],
+    handler: controllers.products,
+  });
 
+  // createProduct (POST) /
+  fastify.route({
+    schema: {
+      response: {
+        200: ProductSchemaRef,
+      },
+      params: {
+        type: "object",
+        properties: {
+          name: { type: "string" },
+          price: { type: "number" },
+          stock: { type: "number" },
+          description: { type: "string" },
+          categories: { type: "array" },
+          images: { type: "string" },
+        },
+      },
+      operationId: "createProduct",
+      tags: ["Products"],
+      security: [
+        {
+          sessionid: [],
+        },
+      ],
+    },
+    method: "POST",
+    url: "",
+    preValidation: [isLoggedIn, isStaff],
+    handler: controllers.products,
+  });
+
+  // deleteProduct (DELETE) /
+  fastify.route({
+    schema: {
+      response: {
+        200: ProductSchemaRef,
+      },
+      params: {
+        type: "object",
+        properties: {
+          productId: { type: "string" },
+        },
+      },
+      operationId: "deleteProduct",
+      tags: ["Products"],
+      security: [
+        {
+          sessionid: [],
+        },
+      ],
+    },
+    method: "DELETE",
+    url: "/:productId",
+    preValidation: [isLoggedIn, isStaff],
+    handler: controllers.products,
+  });
+
+  // deleteProducts (DELETE) /
+  fastify.route({
+    schema: {
+      response: {
+        200: ProductSchemaRef,
+      },
+      params: {
+        type: "object",
+        properties: {
+          productId: { type: "string" },
+        },
+      },
+      operationId: "deleteProducts",
+      tags: ["Products"],
+      security: [
+        {
+          sessionid: [],
+        },
+      ],
+    },
+    method: "DELETE",
+    url: "/:productId",
+    preValidation: [isLoggedIn, isStaff],
+    handler: controllers.products,
+  });
+
+  // updateProduct (PUT) /
+  fastify.route({
+    schema: {
+      response: {
+        200: ProductSchemaRef,
+      },
+      params: {
+        type: "object",
+        properties: {
+          productId: { type: "string" },
+        },
+      },
+      operationId: "updateProduct",
+      tags: ["Products"],
+      security: [
+        {
+          sessionid: [],
+        },
+      ],
+    },
+    method: "PUT",
+    url: "/:productId",
+    preValidation: [isLoggedIn, isStaff],
+    handler: controllers.products,
+  });
 }
