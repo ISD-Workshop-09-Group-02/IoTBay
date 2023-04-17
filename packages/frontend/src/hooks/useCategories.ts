de96b5162e7bd325a4da580180b0574ffdeb2a70
Shaan <shaankhan101@gmail.com>
2023-04-17 14:30:11 +1000

    Updated readme

--- packages/frontend/src/hooks/useCategories.ts
+++ packages/frontend/src/hooks/useCategories.ts
@@ -6,22 +6,6 @@ import {
   CategorySchema,
 } from "../api/generated";
 
-/*
-    How react query works
-
-    We have a unique key for each query, and we can use that key to invalidate the query.
-    data, isLoading, error, refetch, error
-
-
-    mutations
-    useMutation -> we can add onSuccess to invalidate the query category and refresh on demand
-
-    Pagination
-        If it  goes page by page we can use useInfiniteQuery
-        getNextPageParam: (lastPage, pages) => lastPage.nextPage,
-        getPreviousPageParam: (firstPage, pages) => firstPage.previousPage,
-*/
-
 const categoryKey = "category";
 
 // getCategory (GET) /:categoryId
