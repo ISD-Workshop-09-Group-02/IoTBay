e3e7504170b7015e1b34ff7e9295c9ada60ccdef
Shaan <shaankhan101@gmail.com>
2023-04-21 21:07:47 +1000

    Refactored table and removed logs

--- packages/frontend/src/pages/ManageInventory.tsx
+++ packages/frontend/src/pages/ManageInventory.tsx
@@ -29,6 +29,7 @@ import BreadCrumbRoute from "../components/BreadCrumbRoute";
 import TableRow from "../features/IoTDeviceCatalogue/TableRow";
 import PageTitle from "../components/PageTitle";
 import SearchAndFilterNavbar from "../features/IoTDeviceCatalogue/SearchAndFilterNavbar";
+import ProductTable from "../features/IoTDeviceCatalogue/ProductTable";
 
 export default function ManageInventory() {
   const [search, setSearch] = useState("");
@@ -111,54 +112,12 @@ export default function ManageInventory() {
         />
 
         {/* Table */}
-        <Box
-          background={
-            useColorMode().colorMode === "light" ? "gray.100" : "gray.900"
-          }
-          padding={4}
-        >
-          <TableContainer>
-            <Table variant={"simple"}>
-              <Thead>
-                <Tr>
-                  <Th textAlign="center">Image</Th>
-                  <Th textAlign="center">Name</Th>
-                  <Th textAlign="center">Stock</Th>
-                  <Th textAlign="center">Categories</Th>
-                  <Th textAlign="center">Last Updated (DD-MM-YYYY)</Th>
-                  <Th textAlign="center">Price ($)</Th>
-                  <Th textAlign="center">Operations</Th>
-                </Tr>
-              </Thead>
-
-              <Tbody>
-                {getProducts.data.length > 0 &&
-                  getProducts.data.map((element, index) => {
-                    return (
-                      <TableRow
-                        key={element.productId}
-                        productId={element.productId}
-                        name={element.name}
-                        price={element.price}
-                        stock={element.stock}
-                        description={element.description}
-                        image={element.image}
-                        category={element.category}
-                        categoryId={element.categoryId}
-                        lastUpdated={
-                          element.lastUpdated
-                            ? new Date(element.lastUpdated)
-                            : null
-                        }
-                        isSelect={selectedItems.includes(element.productId)}
-                        setSelectedItems={setSelectedItems}
-                      />
-                    );
-                  })}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
+        <ProductTable
+          getProducts={getProducts}
+          selectedItems={selectedItems}
+          setSelectedItems={setSelectedItems}
+          isLightMode={useColorMode().colorMode === "light"}
+        />
       </Stack>
     </Container>
   );
