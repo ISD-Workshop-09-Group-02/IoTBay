f31cf52348b7cae09f80cbac09a2a986d419df81
padishahMinecraftGamer <suhawken@gmail.com>
2023-05-18 19:46:33 +1000

    Co-authored-by: Sebastian Pietschner <sebasptsch@users.noreply.github.com>

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -25,6 +25,8 @@ import type { AppRouter } from "backend/src/routers/root.router";
 import { createTRPCReact } from "@trpc/react-query";
 import SuperJSON from "superjson";
 import BrowseInventory from "./features/IoTPublicCatalogue/pages/BrowseInventory";
+import StaffManagement from "./features/StaffManagement/pages/StaffManagement";
+import CreateStaff from "./features/StaffManagement/pages/CreateStaff";
 
 const queryClient = new QueryClient();
 
@@ -93,6 +95,11 @@ const router = createBrowserRouter([
             path: "users",
             element: <UserManagement />,
           },
+          {
+            path: "staff",
+            element: <StaffManagement />,
+          },
+          
         ],
       },
       {
@@ -102,6 +109,10 @@ const router = createBrowserRouter([
       {
         path: "/register",
         element: <Register />,
+      },
+      {
+        path: "/CreateStaff",
+        element: <CreateStaff />,
       },
       {
         path: "/profile",
